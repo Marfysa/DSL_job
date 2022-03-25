ddb7ac549424576056e3386eea6a0b734b89cce4
Marfysa <marfysa14@gmail.com>
2022-03-26 00:29:52 +0300

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,1 +1,27 @@
-job('MNTLAB-mkurakevich-main-build-job')
+GIT_URL = "https://github.com/Marfysa/DSL_job.git"  
+job('MNTLAB-mkurakevich-main-build-job') {
+  parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Allows user choose from multiple choices')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script("""def gitURL = "$GIT_URL"
+                          def command = "git ls-remote -h \$gitURL"
+                          def proc = command.execute()
+                          proc.waitFor()
+                            if ( proc.exitValue() != 0 ) {
+                              println "Error, \${proc.err.text}"
+                              System.exit(-1)
+                              }
+                       )
+                         def branches = proc.in.text.readLines().collect {
+                           it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
+                         }
+                          return branches
+                         """)
+                fallbackScript()
+            }
+        }
+    }
+}
+
