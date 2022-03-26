94520cad131e19c6a00d9b90acefac6c50cdfcc4
Marfysa <marfysa14@gmail.com>
2022-03-26 10:49:42 +0300

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,4 +1,4 @@
-GIT_URL = "https://github.com/Marfysa/DSL_job.git"  
+def GIT_URL = "https://github.com/Marfysa/DSL_job.git"  
 job('MNTLAB-mkurakevich-main-build-job') {
   parameters {
         activeChoiceParam('BRANCH_NAME') {
@@ -13,7 +13,6 @@ job('MNTLAB-mkurakevich-main-build-job') {
                               println "Error, \${proc.err.text}"
                               System.exit(-1)
                               }
-                       )
                          def branches = proc.in.text.readLines().collect {
                            it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
                          }
@@ -22,6 +21,14 @@ job('MNTLAB-mkurakevich-main-build-job') {
                 fallbackScript()
             }
         }
+      activeChoiceParam('CHILD_JOBS') {
+        description('Jobs for execution')
+          choiceType('CHECKBOX')
+          groovyScript {
+          script('["MNTLAB-mkurakevich-child1-build-job", "MNTLAB-mkurakevich-child2-build-job", "MNTLAB-mkurakevich-child3-build-job", "MNTLAB-mkurakevich-child4-build-job"]')
+          fallbackScript()
+      }
     }
-}
+  }  
+
 
