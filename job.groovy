8bedd9fa022fa7b9715bb1a67be4bb02a68f4c6a
Marfysa <marfysa14@gmail.com>
2022-03-27 11:57:01 +0300

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,34 +1,32 @@
-def GIT_URL = "https://github.com/Marfysa/DSL_job.git"  
+def GIT_URL = "https://github.com/Marfysa/build-t00ls.git"  
+def arr = ["MNTLAB-mkurakevich-child1-build-job", "MNTLAB-mkurakevich-child2-build-job", "MNTLAB-mkurakevich-child3-build-job", "MNTLAB-mkurakevich-child4-build-job"]
 job('MNTLAB-mkurakevich-main-build-job') {
+  description 'Main'
   parameters {
-        activeChoiceParam('BRANCH_NAME') {
-            description('Allows user choose from multiple choices')
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script("""def gitURL = "$GIT_URL"
-                          def command = "git ls-remote -h \$gitURL"
-                          def proc = command.execute()
-                          proc.waitFor()
-                            if ( proc.exitValue() != 0 ) {
-                              println "Error, \${proc.err.text}"
-                              System.exit(-1)
-                              }
-                         def branches = proc.in.text.readLines().collect {
-                           it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-                         }
-                          return branches
-                         """)
-                fallbackScript()
+    activeChoiceParam('BRANCH_NAME') {
+      description('Branch name')
+        choiceType('SINGLE_SELECT')
+        groovyScript {
+        script("""def gitURL = $GIT_URL
+            def command = "git ls-remote -h \$gitURL"
+            def proc = command.execute()
+            proc.waitFor()
+            if ( proc.exitValue() != 0 ) {
+              println "Error, \${proc.err.text}"
+              System.exit(-1)
             }
-        }
-      activeChoiceParam('CHILD_JOBS') {
-        description('Jobs for execution')
-          choiceType('CHECKBOX')
-          groovyScript {
-          script('["MNTLAB-mkurakevich-child1-build-job", "MNTLAB-mkurakevich-child2-build-job", "MNTLAB-mkurakevich-child3-build-job", "MNTLAB-mkurakevich-child4-build-job"]')
-          fallbackScript()
+            def branches = proc.in.text.readLines().collect {
+              it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
+            }
+            return branches
+            """)
+        fallbackScript()
       }
     }
-  }  
-
-
+    activeChoiceReactiveParam('CHILD_NAMES') {
+           description('Childe jobs')
+           choiceType('CHECKBOX')
+           groovyScript {
+               script('return ["MNTLAB-mkurakevich-child1-build-job", "MNTLAB-mkurakevich-child2-build-job", "MNTLAB-mkurakevich-child3-build-job", "MNTLAB-mkurakevich-child4-build-job"]')
+           }
+    }
